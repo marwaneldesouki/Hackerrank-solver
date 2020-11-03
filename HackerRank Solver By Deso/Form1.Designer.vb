9a4c9b95d62630ace74a0139b5f6ab42e22e993f
marwan eldesouki <37198610+marwaneldesouki@users.noreply.github.com>
2020-11-03 02:08:47 +0200

    Add files via upload

---
+++ HackerRank Solver By Deso/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TestThemeEditorForm1 = New HackerRank_Solver_By_Deso.TestThemeEditorForm()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TestThemeEditorForm1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TestThemeEditorForm1
+        '
+        Me.TestThemeEditorForm1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.TestThemeEditorForm1.Colors = New HackerRank_Solver_By_Deso.Bloom(-1) {}
+        Me.TestThemeEditorForm1.Controls.Add(Me.Label2)
+        Me.TestThemeEditorForm1.Controls.Add(Me.Label1)
+        Me.TestThemeEditorForm1.Controls.Add(Me.Button2)
+        Me.TestThemeEditorForm1.Controls.Add(Me.Button1)
+        Me.TestThemeEditorForm1.Controls.Add(Me.TextBox2)
+        Me.TestThemeEditorForm1.Controls.Add(Me.TextBox1)
+        Me.TestThemeEditorForm1.Customization = ""
+        Me.TestThemeEditorForm1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TestThemeEditorForm1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.TestThemeEditorForm1.Image = Nothing
+        Me.TestThemeEditorForm1.Location = New System.Drawing.Point(0, 0)
+        Me.TestThemeEditorForm1.MinimumSize = New System.Drawing.Size(50, 50)
+        Me.TestThemeEditorForm1.Movable = True
+        Me.TestThemeEditorForm1.Name = "TestThemeEditorForm1"
+        Me.TestThemeEditorForm1.NoRounding = False
+        Me.TestThemeEditorForm1.Sizable = False
+        Me.TestThemeEditorForm1.Size = New System.Drawing.Size(301, 326)
+        Me.TestThemeEditorForm1.SmartBounds = True
+        Me.TestThemeEditorForm1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.TestThemeEditorForm1.TabIndex = 0
+        Me.TestThemeEditorForm1.Text = "HackerRank Solver By Deso"
+        Me.TestThemeEditorForm1.TransparencyKey = System.Drawing.Color.Empty
+        Me.TestThemeEditorForm1.Transparent = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.ForeColor = System.Drawing.Color.LightGray
+        Me.Label2.Location = New System.Drawing.Point(9, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Challenge Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.ForeColor = System.Drawing.Color.LightGray
+        Me.Label1.Location = New System.Drawing.Point(10, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Status: ..."
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(161, 108)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(122, 28)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Contact"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(12, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 28)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Get Answer"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.Silver
+        Me.TextBox2.Location = New System.Drawing.Point(9, 142)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(276, 173)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox1.Location = New System.Drawing.Point(135, 39)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(156, 24)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 326)
+        Me.Controls.Add(Me.TestThemeEditorForm1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(50, 50)
+        Me.Name = "Form1"
+        Me.Text = "HackerRank Solver By Deso"
+        Me.TestThemeEditorForm1.ResumeLayout(False)
+        Me.TestThemeEditorForm1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TestThemeEditorForm1 As TestThemeEditorForm
+End Class
---
+++ HackerRank Solver By Deso/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim xstart As Thread = New Thread(AddressOf get_data) : xstart.Start()
+    End Sub
+    Function get_ans(str As String)
+        Try
+            Label1.Text = "Status: Geting Answer..."
+            Label1.ForeColor = Color.Yellow
+            Dim reg As String = Regex.Match(str, "<code class=""language-python"">([^<]*)</code>").Groups(1).Value
+            Dim ans = reg.Replace("&lt;", "<").Replace("&gt;", ">")
+            TextBox2.Text = ans
+            Label1.Text = "Status: This is the answer"
+            Label1.ForeColor = Color.Lime
+        Catch ex As Exception
+            Label1.Text = "Status: Error In Getting ans."
+            Label1.ForeColor = Color.Red
+        End Try
+    End Function
+    Sub get_data()
+        Try
+            Label1.Text = "Status: Geting Data..."
+            Label1.ForeColor = Color.Yellow
+            Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://www.thepoorcoder.com/hackerrank-" + TextBox1.Text.Replace(" ", "-") + "-solution/"), HttpWebRequest)
+            httpWebRequest.Proxy = Nothing
+            httpWebRequest.Method = "GET"
+            httpWebRequest.Headers.Add("Cookie", "__cfduid=d1b1f8190e97795255ee82588af04367a1601746428; ezepvv=0; lp_200442=https://www.thepoorcoder.com/hackerrank-arrays-introduction-solution/; ezCMPCCS=true; __utma=189523054.621902181.1601746483.1601746483.1601746483.1; __utmz=189523054.1601746483.1.1.utmcsr=hackerrank.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmc=189523054; ezds=ffid%3D1%2Cw%3D1536%2Ch%3D864; _ga=GA1.2.621902181.1601746483; _gid=GA1.2.1089439417.1601746484; id5id.1st=%7B%22ID5ID%22%3A%22ID5-ZHMOcgv3hHfLghBCQRA-nBO4fGzrNuDGaWeLB1r1cw%22%2C%22ID5ID_CREATED_AT%22%3A%222020-08-03T20%3A11%3A08Z%22%2C%22ID5_CONSENT%22%3Atrue%2C%22CASCADE_NEEDED%22%3Atrue%2C%22ID5ID_LOOKUP%22%3Atrue%2C%223PIDS%22%3A%5B%5D%7D; id5id.1st_last=Sat%2C%2003%20Oct%202020%2017%3A34%3A56%20GMT; ezosuigeneris=0adefbdc409fe3a680edf5e713a55815; __gads=ID=5e5f299d97279789:T=1601746455:S=ALNI_MaMb8alTKbpLdliJuy34z8Woz37iA; ezouspvh=120; ezux_ifep_200442=true; __qca=P0-713029707-1601746527238; ezoadgid_200442=-1; ezoref_200442=thepoorcoder.com; ezoab_200442=mod54; ezovid_200442=1957530272; ezovuuid_200442=dd38fe1c-ef7c-472c-7d93-2d8e80e9c595; ezohw=w%3D1536%2Ch%3D731; __utmt_e=1; __utmt_f=1; cto_bidid=hITaB19KYzcyc0JkYkZZc3BEWWd2Tkc5c0xYTWpya2xSWW5uRGZwTTZoZFY2N0ladTFxZTNrVVNTUTlxYThqVFB6dkR0UGtiVTl3V05CNW9LMktFZWxZQXk4ZyUzRCUzRA; cto_bundle=lyfAT19FcnRRJTJCb1ZZZzVKJTJGc2I1SnI2dENIREVnSTdlRXFYSm8xNWFBb0o4b1FqQjd1V3MxTnpwMElQVHVFZUhwMHF0RWgzbWlqSjZvM3ZjVHJFR3NKeUdsJTJCd1Z3U0NRV2V2NHRqaXZEUDBtWjVPSEFncWkzR3VaU3BpVHFUQVJwJTJCVCUyRnA; ezosuigenerisc=f6243fcb406936cc8083d783bac3cf7c; active_template::200442=pub_site.1601752941; ezopvc_200442=25; ezovuuidtime_200442=1601752942; _gat_gtag_UA_132753903_12=1; ezux_lpl_200442=1601753010662|787d7b7e-8c16-4286-66e6-385ae5f7e8c6|false; ezouspva=4; ezouspvv=118; __utmb=189523054.250.8.1601753027794; ezux_et_200442=667; ezux_tos_200442=5409")
+            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
+            httpWebRequest.KeepAlive = True
+            Dim httpWebResponse As HttpWebResponse = CType(httpWebRequest.GetResponse(), HttpWebResponse)
+            Dim streamReader As StreamReader = New StreamReader(httpWebResponse.GetResponseStream())
+            Dim input As String = streamReader.ReadToEnd()
+            Label1.Text = "Status: Data Scraped"
+            Label1.ForeColor = Color.Lime
+            get_ans(input)
+        Catch ex As Exception
+            Label1.Text = "Status: Error In Getting Data" & vbCrLf & "(Check name of the challenge)."
+            Label1.ForeColor = Color.Red
+        End Try
+    End Sub
+
+    Private Sub TextBox2_DoubleClick(sender As Object, e As EventArgs) Handles TextBox2.DoubleClick
+
+    End Sub
+
+    Private Sub TestThemeEditorForm1_Click(sender As Object, e As EventArgs) Handles TestThemeEditorForm1.Click
+
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        End
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+
+            Using webClient As WebClient = New WebClient()
+                AddHandler webClient.DownloadStringCompleted, Sub(_sender As Object, _e As DownloadStringCompletedEventArgs)
+                                                                  If _e.[Error] IsNot Nothing Then
+                                                                      MessageBox.Show("Error: No internet connection")
+                                                                  Else
+                                                                      MessageBox.Show(_e.Result)
+                                                                  End If
+
+                                                              End Sub
+                webClient.DownloadStringAsync(New Uri("https://pastebin.com/raw/aCA5PCq9"))
+            End Using
+        Catch ex As Exception : End Try
+    End Sub
+End Class
